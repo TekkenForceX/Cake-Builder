174ca9d2a07376e6add56f3f8b07c8f3df44aace
Amanda Chahine <hello@grandcircus.co>
2025-02-27 20:18:01 -0500

    cake/cake layers components

--- cakebuilder/src/App.tsx
+++ cakebuilder/src/App.tsx
@@ -1,13 +1,16 @@
-//import { useState } from 'react'
+import Layer from './Layer'
 import './App.css'
 import Cake from './Cake/Cake'
 import CakeBuilder from './CakeBuilder/CakeBuilder'
+import { useState } from 'react'
 
 const App = () => {
+  const [layers, setLayers] = useState<Layer[]>([])
+
   return (
     <>
       <CakeBuilder />
-      <Cake />
+      <Cake layers={layers} />
     </>
   )
 }
