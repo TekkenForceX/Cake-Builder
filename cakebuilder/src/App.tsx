ef46bb06f0ea70be95333ec7f3db407858fa641e
Kevin Johnson <johnson.kevin.a@icloud.com>
2025-03-05 21:51:26 -0500

    button & media screen

--- cakebuilder/src/App.tsx
+++ cakebuilder/src/App.tsx
@@ -1,14 +1,14 @@
-import Layer from './Layer'
-import './App.sass'
-import Cake from './Cake/Cake'
-import CakeBuilder from './CakeBuilder/CakeBuilder'
-import { useState } from 'react'
+import Layer from "./Layer"
+import "./App.sass"
+import Cake from "./Cake/Cake"
+import CakeBuilder from "./CakeBuilder/CakeBuilder"
+import { useState } from "react"
 
 const App = () => {
   const [layers, setLayers] = useState<Layer[]>([])
 
   const addLayer = (newLayer: Layer) => {
-    setLayers((ls) => [...ls, newLayer])
+    setLayers(ls => [...ls, newLayer])
   }
 
   const removeLayer = (layer: Layer) => {
@@ -16,10 +16,10 @@ const App = () => {
   }
 
   return (
-    <>
+    <div className='app-container'>
       <CakeBuilder layers={layers} addLayer={addLayer} removeLayer={removeLayer} />
       <Cake layers={layers} />
-    </>
+    </div>
   )
 }
 
--- cakebuilder/src/CakeBuilder/CakeBuilder.tsx
+++ cakebuilder/src/CakeBuilder/CakeBuilder.tsx
@@ -1,34 +1,35 @@
-import Layer from "../Layer";
-import LayerBuilder from "./LayerBuilder";
-import LayerAdd from "./LayerAdd";
-import LayerForm from "./LayerForm";
-import { useState } from "react";
+import Layer from "../Layer"
+import LayerBuilder from "./LayerBuilder"
+import LayerAdd from "./LayerAdd"
+import LayerForm from "./LayerForm"
+import { useState } from "react"
 
 interface CakeBuilderProps {
-    layers: Layer[];
-    addLayer: (newLayer: Layer) => void;
-    removeLayer: (layer: Layer) => void;
+  layers: Layer[]
+  addLayer: (newLayer: Layer) => void
+  removeLayer: (layer: Layer) => void
 }
 
-const CakeBuilder: React.FC<CakeBuilderProps> = ({ layers, addLayer, removeLayer }) => {
-    const [isFormVisible, setIsFormVisible] = useState(false)
+const CakeBuilder: React.FC<CakeBuilderProps> = ({
+  layers,
+  addLayer,
+  removeLayer
+}) => {
+  const [isFormVisible, setIsFormVisible] = useState(false)
 
-    const closeForm = () => {
-        setIsFormVisible(false)
-    }
-    
-    return (
-        <div>
-            <LayerAdd setIsFormVisible={setIsFormVisible} />
-            {isFormVisible && (
-                <LayerForm addLayer={addLayer} closeForm={closeForm} />
-            )}
-            {layers.map((layer) => (
-                <LayerBuilder key={layer.id} layer={layer} removeLayer={removeLayer} />
-            )
-        )}
-        </div>
-    )
+  const closeForm = () => {
+    setIsFormVisible(false)
+  }
+
+  return (
+    <div className='cake-builder'>
+      <LayerAdd setIsFormVisible={setIsFormVisible} />
+      {isFormVisible && <LayerForm addLayer={addLayer} closeForm={closeForm} />}
+      {layers.map(layer => (
+        <LayerBuilder key={layer.id} layer={layer} removeLayer={removeLayer} />
+      ))}
+    </div>
+  )
 }
 
-export default CakeBuilder;
+export default CakeBuilder
